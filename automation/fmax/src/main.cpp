568280b735a5cdbb9383c630ce7547c5350340c8
Emile Badenhorst <myvarhd@gmail.com>
2024-10-19 15:28:50 +0200

    Implemented dumper

--- automation/fmax/src/main.cpp
+++ automation/fmax/src/main.cpp
@@ -5,20 +5,56 @@
 
 int main(int argc, char *argv[]) {
 
-  Yosys::log_streams.push_back(&std::cout);
-  Yosys::log_error_stderr = true;
+  if (argc != 2) {
+    printf("Please provide an rtl directory and output path\n");
+    return -1;
+  }
+
+  auto rtl = argv[1];
+
+  // Yosys::log_streams.push_back(&std::cout);
+  // Yosys::log_error_stderr = true;
   Yosys::yosys_setup();
 
   Yosys::yosys_design = new Yosys::RTLIL::Design;
 
-  Yosys::run_pass("read_verilog ../../rtl/*.v");
-  Yosys::run_pass("read -sv ../../rtl/*.sv");
+  {
+    char buf[255] = {0};
+    sprintf(buf, "read_verilog %s/*.v", rtl);
+    Yosys::run_pass(buf);
+  }
+
+  {
+    char buf[255] = {0};
+    sprintf(buf, "read -sv %s/*.sv", rtl);
+    Yosys::run_pass(buf);
+  }
+
   Yosys::run_pass("prep");
   Yosys::run_pass("opt -full");
 
+  printf("{\n");
   for (auto module : Yosys::yosys_design->modules()) {
-    printf("Foo Name: %s\n", module->name.c_str());
+    printf("\t{\n");
+    printf("\t\t\"name\": \"%s\",\n", module->name.c_str() + 1);
+
+    auto ports = module->ports;
+
+    printf("\t\t\"ports\": [\n");
+    for (auto port : ports) {
+      auto *w = module->wire(port);
+
+      printf("\t\t\t{\n");
+      printf("\t\t\t\t\"name\": \"%s\",\n", port.c_str() + 1);
+      printf("\t\t\t\t\"width\": \%d,\n", w->width);
+      // this does not account for inout ports but not now
+      printf("\t\t\t\t\"input\": \%s\n", w->port_input ? "true" : "false");
+      printf("\t\t\t},\n");
+    }
+    printf("\t\t]\n");
+    printf("\t},\n");
   }
 
+  printf("]\n");
   return EXIT_SUCCESS;
 }
